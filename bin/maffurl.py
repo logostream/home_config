ffd04bcea3f0cfa08034d1dd3cc20a3afc6a346a
Stream Liu <logostream@gmail.com>
2013-12-22 17:33:04 +0800

    tools to extract urls from archived page

---
+++ bin/maffurl.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+import zipfile as zf;
+import sys;
+import re;
+
+def maffurl (maffpath):
+    got = 0;
+    with zf.ZipFile(maffpath) as maffzip:
+        for filename in maffzip.namelist():
+            if re.match('^\w+/index.rdf$', filename) is None:
+                continue;
+            with maffzip.open(filename) as rdf:
+                for line in rdf:
+                    match = re.search('<MAF:originalurl RDF:resource="(.*)"/>', line);
+                    if not match is None:
+                        print match.group(1);
+                        got += 1;
+    sys.stderr.write("Got %d urls.\n" % got);
+    return;
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: %s ${maff_path}\n", sys.argv[0]);
+    maffurl(sys.argv[1]);
---
+++ bin/mhturl.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+import sys;
+import re;
+
+def mhturl (mhtpath):
+    got = 0;
+    with open(mhtpath) as mht:
+        for line in mht:
+            match = re.search('^Content-Location: (.*)$', line);
+            if not match is None:
+                print match.group(1);
+                got += 1;
+    sys.stderr.write("Got %d urls.\n" % got);
+    return;
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: %s ${mht_path}\n", sys.argv[0]);
+    mhturl(sys.argv[1]);
+
