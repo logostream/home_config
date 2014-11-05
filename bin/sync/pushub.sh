34392e1bbc5e43ba10705d4c1996e9c4376d4996
Stream Liu <logostream@gmail.com>
2014-11-04 21:39:35 -0800

    sync script for pushub

---
+++ bin/sync/pushub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Synopsis:
+#     :$ mirror_sync.sh
+#     enter password:
+#     ^Z
+#     :$ bg %1
+#     :$ cat ~/log/mirror_sync.err # check error log
+#     :$ tail -f ~/log/mirror_sync.log # check log
+# Discription: sync whole repositories between mirrors
+
+CELL=gmacbook
+SERVER_ADDR=stream@foo
+SERVER_DIR=pushub/$CELL
+CLIENT_DIR=$HOME/pushub
+if [ ! -d $CLIENT_DIR ] ; then
+	echo "Client directory $CLIENT_DIR doesn't exist"
+	exit 1
+fi
+if [[ `ssh $SERVER_ADDR test -d $SERVER_DIR || echo not exist` ]] ; then
+	echo "Server directory $SERVER_DIR doesn't exist"
+	exit 1
+fi
+
+if [[ $1 == "-b" ]] ; then
+	rsync -avvH --delete "$SERVER_ADDR:$SERVER_DIR/" "$CLIENT_DIR" > ~/log/pushub.log 2> ~/log/pushub.err
+else
+	rsync -avvH --delete "$CLIENT_DIR/" "$SERVER_ADDR:$SERVER_DIR" > ~/log/pushub.log 2> ~/log/pushub.err
+fi
